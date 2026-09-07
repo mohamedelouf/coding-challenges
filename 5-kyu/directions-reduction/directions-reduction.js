6329870e1e083424189ecf350bb1450b27ae1d56
MohamedElouf <mohamedelouf1120@gmail.com>
2026-09-08 02:16:44 +0300

    Update solution - CodeHub

--- 5-kyu/directions-reduction/directions-reduction.js
+++ 5-kyu/directions-reduction/directions-reduction.js
@@ -14,10 +14,16 @@ let ofarr={
 for(let i=0; i<arr.length;i++){
   if(ofarr[arr[i]] === arr[i+1]){
     arr.splice(i,2);
-     i=-1;
-  }
-}
+     i=-1;}}
   return arr;*/
 //solution two
-​
 let stack=[];
+for(let i=0 ;i<arr.length;i++){
+ let lastStackItem=stack[stack.length-1];
+ if(ofarr[arr[i]] === lastStackItem){
+    stack.pop();}
+ else{
+  stack.push(arr[i]);}}
+   return stack;}
+​
+​
