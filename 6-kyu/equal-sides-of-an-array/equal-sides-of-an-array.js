1f1fc8dabc15acb25e65e8906976ce4bcae009da
MohamedElouf <mohamedelouf1120@gmail.com>
2026-09-11 20:16:59 +0300

    Update solution - CodeHub

--- 6-kyu/equal-sides-of-an-array/equal-sides-of-an-array.js
+++ 6-kyu/equal-sides-of-an-array/equal-sides-of-an-array.js
@@ -1,21 +1,16 @@
 function findEvenIndex(arr) {
-let rigthtSum=0;
-let leftSum=0;
-for(let i= 0; i<arr.length ;i++){
-    if(i===0){
-        let sum1=0;
-        for(let j=1;j<arr.length;j++){
-          sum1+=arr[j];}
-        if(sum1===0){
-            return i;}}
-    else{
-        for(let x=i+1;x<arr.length;x++){
-            rigthtSum +=arr[x];  }
-        for(let m=i-1;m>=0;m--){
-            leftSum +=arr[m];  }
-  
-    if(rigthtSum===leftSum){
-         return i;}       
-        rigthtSum=0;
-        leftSum=0;}}
+ let totalSum=0;
+ let rightSum=0;
+ let leftSum=0;
+ for(let num of arr){  
+    totalSum += num;
+ }
+ for(let i=0;i<arr.length;i++){
+​
+     rightSum=totalSum-leftSum-arr[i];
+     if(rightSum===leftSum){
+       return i;
+     }
+     leftSum += arr[i];
+ }
     return -1;}
